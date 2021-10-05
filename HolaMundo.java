2870320f1e64741ae7452822f22367ca29eab763
Suseth Sandoval <91089461+SaBiSand@users.noreply.github.com>
2021-10-05 14:20:33 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,17 @@
+public class HolaMundo {
+	/*
+	 * Aplicacion Java
+	 * Title	:Hola Mundo en Java
+	 * Description	:Hola Mundo en java desde vim en Windows
+	 * Author	:Suseth Abigail Sandoval Damian
+	 * Date		:20211004
+	 * Version	:1
+	 * Usage	:c:\>javac /path/HolaMundo.java
+	 *
+	 * Hola mundo en Java
+	 */
+
+	public static void main(String []args){
+		System.out.println("Hola Mundo por segunda vez en Java");
+	}
+}
